1f9784087aa188a7b5ff8479acb998615906600f
AlexKVal <alexkval@gmail.com>
2016-07-07 15:51:02 +0300

    fake some data and use Article component

--- web/elm/Components/ArticleList.elm
+++ web/elm/Components/ArticleList.elm
@@ -2,15 +2,31 @@ module Components.ArticleList exposing (view)
 
 import Html exposing (Html, text, ul, li, div, h2)
 import Html.Attributes exposing (class)
+import List
+import Components.Article as Article
+
+
+articles : List Article.Model
+articles =
+    [ { title = "Article 1", url = "http://google.com", postedBy = "Author 1", postedOn = "07/07/16" }
+    , { title = "Article 2", url = "http://google.com", postedBy = "Author 2", postedOn = "07/07/16" }
+    , { title = "Article 3", url = "http://google.com", postedBy = "Author 3", postedOn = "07/07/16" }
+    ]
+
+
+renderArticle : Article.Model -> Html a
+renderArticle article =
+    li [] [ Article.view article ]
+
+
+renderArticles : List (Html a)
+renderArticles =
+    List.map renderArticle articles
 
 
 view : Html a
 view =
     div [ class "article-list" ]
         [ h2 [] [ text "Article List" ]
-        , ul []
-            [ li [] [ text "Article 1" ]
-            , li [] [ text "Article 2" ]
-            , li [] [ text "Article 3" ]
-            ]
+        , ul [] renderArticles
         ]
